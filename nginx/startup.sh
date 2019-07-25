36448d5f8ee86a54d43349cc7d4b8eff131e7b6d
linc <qulamj@gmail.com>
2019-07-25 18:36:59 +0800

    fix: 誤刪

--- nginx/startup.sh
+++ nginx/startup.sh
@@ -5,3 +5,5 @@ if [ ! -f /etc/nginx/ssl/default.crt ]; then
     openssl req -new -key "/etc/nginx/ssl/default.key" -out "/etc/nginx/ssl/default.csr" -subj "/CN=default/O=default/C=UK"
     openssl x509 -req -days 365 -in "/etc/nginx/ssl/default.csr" -signkey "/etc/nginx/ssl/default.key" -out "/etc/nginx/ssl/default.crt"
 fi
+
+nginx
