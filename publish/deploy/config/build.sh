9cd4c3d545a6e7cebc3c7142ab50a1f87125156f
linc <qulamj@gmail.com>
2019-09-19 17:49:28 +0800

    fix: custom build 設定

--- publish/deploy/config/build.sh
+++ publish/deploy/config/build.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
 if [ ${APP_ENV} = 'PRODUCTION' ]; then \
-  composer install;
+  echo 'Is production';
+  composer install --no-dev;
 fi
 
 if [ ${APP_ENV} = 'STAGING' ]; then \
-  composer install --no-dev;
+  echo 'Is staging';
+  composer install;
+fi
+
+if [ ${APP_ENV} = 'DEVELOP' ]; then \
+  echo 'Is develop';
 fi
